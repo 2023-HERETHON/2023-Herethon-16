0ad2c7b0d408dbf6574cad16e7c1e10f8af86725
kknari <nari907@duksung.ac.kr>
2023-07-13 16:31:17 +0900

    get 기능 완성, post 보완 필요

--- my_project/my_project/posts/models.py
+++ my_project/my_project/posts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(verbose_name='제목', max_length=20)
+    destination = models.CharField(verbose_name='여행지', max_length=20)
+    # 여행 시작 ~ 끝 기간
+    startPeriod = models.DateField(verbose_name='여행시작')
+    endPeriod = models.DateField(verbose_name='여행끝')
+    created_at = models.DateTimeField(verbose_name='작성일', auto_now_add=True)
+    content = models.TextField(verbose_name='상세일정')
+    record = models.TextField(verbose_name='여행기록')
+
+    like_count = models.IntegerField(verbose_name='좋아요개수', null=True, blank=True)
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='likes', null=True, blank=True)
+
+    def __str__(self): 
+        return self.title
+    
+    def get_absolute_url(self):
+        return f'posts/{self.pk}/'
+    
--- my_project/my_project/posts/views.py
+++ my_project/my_project/posts/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from .models import Post
+from django.http import HttpResponseRedirect
+from django.utils import timezone
+from django.urls import reverse
 
-# Create your views here.
+def Post_list(request):
+    postlist = Post.objects.all()
+    return render(request, 'mytrip.html', {'postlist' : postlist})
+
+def Post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'mytrip_post.html', {'post': post})
+
+def detail(request):
+    return render(request, 'mytrip_detail.html')
+
+def write(request):
+    postlist = Post.objects.all()
+    if request.method == 'POST':
+        b = Post(
+            title = request.POST['title'],
+            destination = request.POST['destination'],
+            startPeriod = request.POST['startPeriod'],
+            endPeriod = request.POST['endPeriod'],
+            content = request.POST['content'],
+            record = request.POST['record'],
+            created_at = timezone.now()
+        )
+        b.save()
+    return HttpResponseRedirect('mytrip.html')
